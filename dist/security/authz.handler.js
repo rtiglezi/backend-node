26a9bae4db5b77f26647c43947e9ff6fc85a17ce
Ronaldo Tonioli Iglezias <ronaldotonioli@gmail.com>
2019-07-05 10:21:41 -0300

    auth little atualization

--- dist/security/authz.handler.js
+++ dist/security/authz.handler.js
@@ -3,7 +3,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const restify_errors_1 = require("restify-errors");
 exports.authorize = (...profiles) => {
     return (req, resp, next) => {
-        console.log("req.authenticated", req.authenticated);
         if (req.authenticated !== undefined && req.authenticated.hasAny(...profiles)) {
             req.log.debug('User %s is authorized with profiles %j on route %s. Required profiles %j', req.authenticated._id, req.authenticated.profiles, req.path(), profiles);
             next();

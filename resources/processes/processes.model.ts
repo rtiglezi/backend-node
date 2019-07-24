0be0db3ce267162aee6e23f14926f5630b3a9132
Ronaldo Tonioli Iglezias <ronaldotonioli@gmail.com>
2019-07-23 23:37:26 -0300

    api CNPJ

--- resources/processes/processes.model.ts
+++ resources/processes/processes.model.ts
@@ -1,3 +1,4 @@
+import { Request } from './../../index.d';
 import * as mongoose from 'mongoose';
 
 import { Tenant } from '../tenants/tenants.model';
@@ -5,22 +6,12 @@ import { Demand } from '../demands/demands.model';
 import { Division } from '../divisions/divisions.model';
 import { User } from '../users/users.model';
 
-import { validateCPF } from '../../common/validators'
-
 
 export interface Requester extends mongoose.Document {
     person: string,
     document: string,
     name: string,
-    tradingName: string,
-    addressPublicArea: string,
-    addressNumber: string,
-    addressComplement: string,
-    addressNeighborhood: string,
-    addressZipCode: string,
-    addressCity: string,
-    addressState: string,
-    email: string
+    tradingName: string
 }
 
 export interface Process extends mongoose.Document {
@@ -29,7 +20,9 @@ export interface Process extends mongoose.Document {
     division: mongoose.Types.ObjectId | Division
     user: mongoose.Types.ObjectId | User
     requester: mongoose.Types.ObjectId | Requester,
-    number: string
+    number: string,
+    city: string,
+    state: string
 }
 
 
@@ -53,38 +46,6 @@ const requesterSchema = new mongoose.Schema({
         type: String,
         maxlength: 30,
         minlength: 3
-    },
-    addressPublicArea: {
-        type: String,
-        required: false
-    },
-    addressNumber: {
-        type: String,
-        required: false
-    },
-    addressComplement: {
-        type: String,
-        required: false
-    },
-    addressNeighborhood: {
-        type: String,
-        required: false
-    },
-    addressZipCode: {
-        type: String,
-        required: false
-    },
-    addressCity: {
-        type: String,
-        required: false
-    },
-    addressState: {
-        type: String,
-        required: false
-    },
-    email: {
-        type: String,
-        match: /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
     }
 })
 
@@ -115,6 +76,14 @@ const processSchema = new mongoose.Schema({
     number: {
         type: String,
         required: true,
+    },
+    city: {
+        type: String,
+        required: true,
+    },
+    state: {
+        type: String,
+        required: true
     }
 },
     {
